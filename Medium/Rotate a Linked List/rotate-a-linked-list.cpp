9eb679fc535ab0900671d9e5bc7b0b3dcfc28802
Priyanshu Agnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2024-05-14 23:57:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotate a Linked List/rotate-a-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+           int length = 0;
+        Node *tr = head;
+        while (tr!=NULL){
+            length++;
+            tr = tr->next;
+        }
+        if (k==0||k==length){
+            return head;
+        }
+        int cnt = 0;
+        Node *cur = head;
+        while (cnt<k-1){
+            cnt++;
+            cur = cur -> next;
+        }
+        Node *last = head;
+        Node *temp = cur ->next;
+        while (last->next!=NULL){
+            last = last->next;
+        }
+        last -> next = head;
+        cur->next = NULL;
+        return temp;
+    }
+};
+    
+
+
+//{ Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+
+// } Driver Code Ends
