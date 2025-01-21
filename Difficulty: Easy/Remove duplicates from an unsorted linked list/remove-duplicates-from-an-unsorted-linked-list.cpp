731dd3caa5dbd780adb89c18f4b8c816f92852ab
Priyanshu Agnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2025-01-21 23:57:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x)
+        : data(x)
+        , next(nullptr) {}
+};
+
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        // your code goes here
+              if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    unordered_set<int> seen;  // To keep track of encountered values
+    Node* current = head;
+    Node* prev = NULL;
+
+    while (current != NULL) {
+        // If current node's data is already in the set, it's a duplicate
+        if (seen.find(current->data) != seen.end()) {
+            // Remove the node by linking the previous node to the next node
+            prev->next = current->next;
+            delete current;
+        } else {
+            // Add the data to the set and move prev pointer
+            seen.insert(current->data);
+            prev = current;
+        }
+        // Move to the next node
+        current = current->next;
+    }
+
+    return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << "empty" << endl;
+            continue;
+        }
+
+        Node *head = new Node(arr[0]);
+        Node *tail = head;
+
+        for (size_t i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *result = ob.removeDuplicates(head);
+        printList(result);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
