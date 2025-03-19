55adb76af2c4d2d0d2bcc57ce72a45d95f48378f
Priyanshu Agnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2025-03-19 19:14:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+/*
+// Tree Node
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool isSymmetric(Node* root) {
+        // Code here
+        if(!root) return true;
+	    queue<Node*>q;
+	    q.push(root->left);
+	    q.push(root->right);
+	    while(!q.empty()){
+	        int n = q.size();
+	        vector<int>v;
+	        while(n--){
+	            Node * node = q.front();
+	            q.pop();
+	            if(node){
+	                v.push_back(node->data);
+	                q.push(node->left);
+	                q.push(node->right);
+	            }
+	            else{
+	                v.push_back(INT_MIN);
+	            }
+	        }
+	        int i = 0;
+	        int j = v.size()-1;
+	        while(i < j){
+	            if(v[i] != v[j]){
+	                return false;
+	            }
+	            i++;
+	            j--;
+	        }
+	    }
+	    return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+
+        Node* root = buildTree(s);
+
+        vector<int> ans;
+        Solution ob;
+        if (ob.isSymmetric(root)) {
+            cout << "True" << endl;
+        } else {
+            cout << "False" << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
